285ac0f1eb5e669948c66769afeee931587eead5
Liudmila Parfenova <liudmila.khab@gmail.com>
2025-03-19 18:05:52 +0100

    Add first API test for Rijksmuseum collection

--- rijksmuseum-api-tests/src/test/java/com/rijksmuseum/api/tests/CollectionTests.java
+++ rijksmuseum-api-tests/src/test/java/com/rijksmuseum/api/tests/CollectionTests.java
@@ -1,9 +1,20 @@
 package com.rijksmuseum.api.tests;
 
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class CollectionTest {
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class CollectionTests {
 
     @Test
-    public void 
+    public void getCollection() {
+        Response response = given()
+                .when()
+                .get("https://www.rijksmuseum.nl/api/nl/collection?key=0fiuZFh4&involvedMaker=Rembrandt+van+Rijn");
+        Assert.assertEquals(response.statusCode(), 200, "Expected status code 200");
+    }
 }
