aa9c46f3bd9d8717a7d8e953017e6a7e07be0afc
Liudmila Parfenova <liudmila.khab@gmail.com>
2025-03-20 15:25:53 +0100

    Add Configuration files and BaseTest, update CollectionTests

---
+++ rijksmuseum-api-tests/src/test/java/com/rijksmuseum/api/configuration/Configuration.java
@@ -0,0 +1,18 @@
+package com.rijksmuseum.api.configuration;
+
+public class Configuration extends AbstractConfiguration{
+
+    @Override
+    protected String getConfigPath() {
+        return "src/test/resources/general.properties";
+    }
+
+    @Override
+    public void loadProperties() {
+
+    }
+
+    public String getBaseUrl(){
+        return getProperty("base.url");
+    }
+}
--- rijksmuseum-api-tests/src/test/java/com/rijksmuseum/api/tests/CollectionTests.java
+++ rijksmuseum-api-tests/src/test/java/com/rijksmuseum/api/tests/CollectionTests.java
@@ -1,20 +1,26 @@
 package com.rijksmuseum.api.tests;
 
 import io.qameta.allure.Story;
-import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.testng.Assert.assertEquals;
+import static org.apache.http.HttpStatus.*;
 
 @Story("RM-1 RM API: GetCollections")
-public class CollectionTests {
+public class CollectionTests extends BaseTest {
 
     @Test(description = "GET /collection", groups = {"smoke", "collections"})
     public void getCollection() {
-        Response response = given()
+
+        ValidatableResponse response1 = given()
+                .spec(getRequestSpec())
+                .queryParam("involvedMaker", "Rembrandt van Rijn")
                 .when()
-                .get("https://www.rijksmuseum.nl/api/nl/collection?key=0fiuZFh4&involvedMaker=Rembrandt+van+Rijn");
-        assertEquals(response.statusCode(), 200, "Expected status code 200");
+                .get("/collection")
+                .then()
+                .log().body()
+                .assertThat()
+                .statusCode(SC_OK);
     }
 }
