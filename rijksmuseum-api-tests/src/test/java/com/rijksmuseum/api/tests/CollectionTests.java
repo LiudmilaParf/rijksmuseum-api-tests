039248e970cd726d554f03825da8225f42ad8893
Liudmila Parfenova <liudmila.khab@gmail.com>
2025-03-20 13:23:35 +0100

    Add Allure report, update gitignore

--- rijksmuseum-api-tests/src/test/java/com/rijksmuseum/api/tests/CollectionTests.java
+++ rijksmuseum-api-tests/src/test/java/com/rijksmuseum/api/tests/CollectionTests.java
@@ -1,20 +1,20 @@
 package com.rijksmuseum.api.tests;
 
+import io.qameta.allure.Story;
 import io.restassured.response.Response;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.Map;
-
 import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
 
+@Story("RM-1 RM API: GetCollections")
 public class CollectionTests {
 
-    @Test
+    @Test(description = "GET /collection", groups = {"smoke", "collections"})
     public void getCollection() {
         Response response = given()
                 .when()
                 .get("https://www.rijksmuseum.nl/api/nl/collection?key=0fiuZFh4&involvedMaker=Rembrandt+van+Rijn");
-        Assert.assertEquals(response.statusCode(), 200, "Expected status code 200");
+        assertEquals(response.statusCode(), 200, "Expected status code 200");
     }
 }
